7e7d603a4a14eb7a822084f4d778a58af0c3badc
Moneil97 <cam8059@yahoo.com>
2016-04-07 07:43:57 -0500

    Added Programmable Distance

--- MotorTester/MotorTester.ino
+++ MotorTester/MotorTester.ino
@@ -1,16 +1,8 @@
-/*     Simple Stepper Motor Control Exaple Code
- *      
- *  by Dejan Nedelkovski, www.HowToMechatronics.com
- *  
- */
-// defines pins numbers
 const int stepPin = 3; 
 const int dirPin = 4; 
 const int buttonPin = 5;
-
  
 void setup() {
-  // Sets the two pins as Outputs
   pinMode(stepPin,OUTPUT); 
   pinMode(dirPin,OUTPUT);
   pinMode(buttonPin,INPUT);
---
+++ ProgrammableDistance/ProgrammableDistance.ino
@@ -0,0 +1,43 @@
+// defines pins numbers
+const int stepPin = 3; 
+const int dirPin = 4; 
+const int buttonPin = 5;
+const int ledPin = 13;
+
+const int distance = 5 * 100; //Distance in Centimeters (m * 100 = cm)
+const int wheelCircumfrence = 30; //Wheel Circumfrence in Centimeters
+const int revs = distance / wheelCircumfrence;
+const int stepsPerRev = 200;
+
+void setup() {
+  pinMode(stepPin,OUTPUT); 
+  pinMode(dirPin,OUTPUT);
+  pinMode(buttonPin,INPUT);
+  //pinMode(ledPin, OUTPUT);
+  forward();
+}
+void loop() {
+  if (digitalRead(buttonPin) == HIGH){
+
+    for(int x = 0; x < stepsPerRev * revs; x++) {
+    digitalWrite(stepPin,HIGH); 
+    delayMicroseconds(500); 
+    digitalWrite(stepPin,LOW); 
+    delayMicroseconds(500); 
+  }
+
+    
+    digitalWrite(stepPin,HIGH); 
+    delayMicroseconds(500); 
+    digitalWrite(stepPin,LOW); 
+    delayMicroseconds(500);
+  }
+}
+
+void forward(){
+  digitalWrite(dirPin,HIGH);
+}
+
+void reverse(){
+  digitalWrite(dirPin,LOW);
+}
